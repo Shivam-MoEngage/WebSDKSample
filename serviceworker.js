a085c0828a3e5b28ba5c1343f6dcd5d6029b40d3
Shivam-MoEngage <shivam.agrawal@moengage.com>
2022-11-15 14:59:27 +0530

    Initial Commit

---
+++ serviceworker.js
@@ -0,0 +1,6 @@
+// importScripts("https://cdn.moengage.com/webpush/releases/serviceworker_cdn.min.latest.js");
+if ("undefined" === typeof window) {
+  importScripts(
+    "https://cdn.moengage.com/webpush/releases/serviceworker_cdn.min.latest.js"
+  );
+}
